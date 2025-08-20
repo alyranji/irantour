c3bbd1e577c652c040ab2462bcdc921e4b828f04
Ali Ranjbaran <ali.ranjbaran76@gmail.com>
2025-08-20 21:06:24 +0330

    add cta

---
+++ app/components/cta.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import Image from "next/image";
+import isf from "@/public/isf.jpg";
+
+function Cta() {
+  return (
+    <div className="bg-[#EBF1F7] my-8">
+      <section className="max-w-[1200px] mx-auto py-8 flex flex-row justify-between items-center gap-8">
+        <div className="flex-1 flex flex-col gap-5">
+          <h3 className="text-2xl font-bold">
+            <span className="text-[#FF8300]">بهترین سفرتان</span> را تجربه کنید
+          </h3>
+          <p className="text-xl">
+            تور ویژه به ایروان اکنون برای شما محیاست تا با خانواده تان سفری پر
+            شور و هیجان داشته باشید. این سفر فوق العاده در تاریخ جذاب پیش روی
+            شما رخ خواهد داد و برای رزرو هرچه سریعتر با همکاران ما در تماس
+            باشید.
+          </p>
+          <button className="py-2 px-10 bg-[#FF8300] rounded-lg text-white w-fit">
+            بزن بریم!
+          </button>
+        </div>
+        <div className="flex items-center">
+          <Image
+            src={isf.src}
+            alt="isfahan"
+            width={500}
+            height={500}
+            className="rounded-full w-[400px] h-[400px]"
+          />
+        </div>
+      </section>
+    </div>
+  );
+}
+
+export default Cta;
--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import Poster from "./components/poster";
 import Advantages from "./components/advantages";
 import SearchBox from "./components/searchBox";
 import Packages from "./components/packages";
+import Cta from "./components/cta";
 export default function Home() {
   return (
     <>
@@ -11,6 +12,7 @@ export default function Home() {
       <Advantages />
       <SearchBox />
       <Packages />
+      <Cta />
     </>
   );
 }
